f6c32b29b39b6b2b4f7132aa360c4a2e2a6635d9
alaa-aleryani <alaaaleryani31@gmail.com>
2023-12-13 09:23:33 -0500

    Updates

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-# Import dependencies
-from flask import Flask, json, jsonify, render_template
-import json
+from flask import Flask, jsonify, render_template
 import requests
-import csv
 
 app = Flask(__name__)
+
 @app.route('/')
-def index():
+def welcome():
+    return (
+        f"/json <br/>"
+        f"/ny_map_and_chart <br/>"
+        # f"/amanda_js <br/>"
+        # f"/alaa_js <br/>"
+    )
+
+@app.route('/json')
+def json_get():
     # Make GET request to API endpoint
-    response = requests.get('https://data.ny.gov/resource/fymg-3wv3')
-    #JSON response
-    data = response.json() ## making the data in json format
+    response = requests.get('https://data.ny.gov/resource/fymg-3wv3.json')
+    # JSON response
+    data = response.json()
     # Return JSON data as JSONified Flask response
     return jsonify(data)
+
+@app.route('/ny_map_and_chart')
+def charts():
+    return render_template("tiffany.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
